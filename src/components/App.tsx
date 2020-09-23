5d2215ef89900560195fe15fd9577e812e593831
WolfMAN <aria.azadi.pour@protonmail.com>
2020-09-23 18:49:23 +0330

    instruction page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import {
-    BrowserRouter as Router,
+    Router,
     Switch,
     Route
 } from "react-router-dom";
 
+import history from "../history";
 import Start from "./Start";
 import Instruction from "./Instruction";
 
 function App() {
     return (
-        <Router>
-            <Switch>
-                <Route path="/">
-                    <Start/>
-                </Route>
-                <Route path="/instruction">
-                    <Instruction/>
-                </Route>
-            </Switch>
+        <Router history={history}>
+            <div>
+                <Switch>
+                    <Route path="/instruction">
+                        <Instruction/>
+                    </Route>
+                    <Route path="/">
+                        <Start/>
+                    </Route>
+                </Switch>
+            </div>
         </Router>
     );
 }
--- src/components/Instruction.tsx
+++ src/components/Instruction.tsx
@@ -1,8 +1,17 @@
-import React, {FC} from "react";
+import React, {FC, useEffect} from "react";
+
+import history from "../history";
 
 const Instruction: FC = () => {
+    useEffect(() => {
+        setTimeout(() => {
+            history.push('/ready/1')
+        }, 1700);
+    }, []);
+
     return (
         <div className="route">
+            خود انتقا گری
         </div>
     );
 };
